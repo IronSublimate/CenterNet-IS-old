0123024b05f065da8c1a70ed66e9e5182e0219e6
IronSublimate <houyuxuan3120487@163.com>
2020-04-03 12:19:37 +0800

    修改voc2007resnet训练脚本

--- experiments/voc2007_resdcn101.sh
+++ experiments/voc2007_resdcn101.sh
@@ -5,7 +5,7 @@ cd src
 if test "$1" == 'train'  ;then
   python main.py ctdet --exp_id voc2007_resdcn101 --batch_size 16 --dataset voc2007 --gpus 5,6 --arch resdcn_101 --load_model ../models/ctdet_pascal_resdcn101_512.pth
 elif test "$1" == 'test'  ;then
-  python test.py --exp_id voc2007_resdcn101 --not_prefetch_test ctdet --load_model ../exp/ctdet/voc2007_resdcn101/model_best.pth --dataset voc2007
+  python test.py --exp_id voc2007_resdcn101 --not_prefetch_test ctdet --load_model ../exp/ctdet/voc2007_resdcn101/model_best.pth --dataset voc2007 --arch resdcn_101
 elif test "$1" == 'show' ;then
-  python mydemo.py ctdet --exp_id voc2007_resdcn101 --demo ~/code/senior/data/VOC2007/JPEGImages  --load_model ../exp/ctdet/voc2007_resdcn101/model_best.pth --dataset voc2007
+  python mydemo.py ctdet --exp_id voc2007_resdcn101 --demo ~/code/senior/data/VOC2007/JPEGImages  --load_model ../exp/ctdet/voc2007_resdcn101/model_best.pth --dataset voc2007 --arch resdcn_101
 fi
