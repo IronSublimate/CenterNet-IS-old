86bcf3e49cec25ac1d1f2bcd03fd9314c90e2cd2
IronSublimate <houyuxuan3120487@163.com>
2020-04-22 17:41:38 +0800

    修改coco resdcn101脚本

--- experiments/ctdet_coco_resdcn101.sh
+++ experiments/ctdet_coco_resdcn101.sh
@@ -1,8 +1,10 @@
 cd src
 # train
-python main.py ctdet --exp_id coco_resdcn101 --arch resdcn_101 --batch_size 16 --master_batch 5 --lr 3.75e-4 --gpus 6,7 --num_workers 16
-# test
-#python test.py ctdet --exp_id coco_resdcn101 --keep_res --resume
+if test "$1" == 'train'  ;then
+    python main.py ctdet --exp_id coco_resdcn101 --arch resdcn_101 --batch_size 16 --master_batch 5 --lr 3.75e-4 --num_workers 16 --load_model ../exp/ctdet/coco_resdcn101/model_best.pth
+elif test "$1" == 'test'  ;then
+    python test.py ctdet --exp_id coco_resdcn101 --keep_res --resume --load_model ../exp/ctdet/coco_resdcn101/model_best.pth --arch resdcn_101
+fi
 # flip test
 #python test.py ctdet --exp_id coco_resdcn101 --keep_res --resume --flip_test 
 # multi scale test
