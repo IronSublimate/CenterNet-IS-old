80d0ac8329e3233d6e7382c64b4fcef8e45aa5a6
IronSublimate <houyuxuan3120487@163.com>
2020-04-29 09:43:01 +0800

    修改coae命名

--- src/lib/datasets/sample/coae.py
+++ src/lib/datasets/sample/coae.py
@@ -10,8 +10,20 @@ from utils.image import gaussian_radius, draw_umich_gaussian, draw_msra_gaussian
 from utils.image import draw_dense_reg
 import math
 from datasets.base import BaseDataset
+from typing import Dict, List, Any
 
 
 class CoAEDataset(BaseDataset):
-    def __getitem__(self, index: int):
-        pass
+    def __getitem__(self, index: int) -> Dict[str, Any]:
+        img_id = self.images[index]
+        file_name = self.coco.loadImgs(ids=[img_id])[0]['file_name']
+        img_path = os.path.join(self.img_dir, file_name)
+        ann_ids = self.coco.getAnnIds(imgIds=[img_id])
+        anns = self.coco.loadAnns(ids=ann_ids)
+        num_objs = min(len(anns), self.max_objs)
+
+        img = cv2.imread(img_path)
+
+        ret = {}
+        ret["input"] = img
+        return ret
